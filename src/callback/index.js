88cdb8795f34d3e5c3a19dbf8f6594252161bbb4
Bryan Valentierra Talero <b.talero27@gmail.com>
2022-05-21 20:49:56 -0500

    Aprendi y use el callback

---
+++ src/callback/index.js
@@ -0,0 +1,25 @@
+// Acá nos pusimos a hacer ejemplos de como podemos usar un callback y en el segundo ejemplo usamos una funcion como argumento para otra funcion
+function sum(num1, num2) {
+    return num1 +    num2;
+};
+
+function calc (num1, num2, callback) {
+    return callback(num1, num2);
+}
+ console.log(calc(6,2, sum));
+
+
+
+ function date (callback) {
+    console.log(new Date);
+    setTimeout(function () {
+        let date = new Date;
+        callback(date);
+    }, 3000)
+ }
+
+ function printDate (dateNow) {
+    console.log(dateNow);
+ };
+
+date(printDate);
