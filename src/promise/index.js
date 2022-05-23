c07e6e98c7054030f0826124b0593e614c138128
Bryan Valentierra Talero <b.talero27@gmail.com>
2022-05-22 22:23:35 -0500

    Aca hicimos peticiones a nuestra API mediante promesas

--- src/promise/index.js
+++ src/promise/index.js
@@ -30,6 +30,6 @@ somethingWillHappen2()
 .then(response => console.log(response))
 .catch(err =>console.log(err));
 
-Promise.all([somethingWillHappen(), somethingWillHappen2()])
+Promise.all([somethingWillHappen(), somethingWillHappen2()]) // Con este .all podemos concatenar varioas promesas poniendo como parametros las variables donde estan las promesas que queremos que se ejecuten a la vez
 .then(response => console.log('Array of results', response))
 .catch(err => {console.log(err)});
