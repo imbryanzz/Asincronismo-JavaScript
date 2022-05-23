ce4fb67c8627c22b84107c6546465a58a9688f5a
Bryan Valentierra Talero <b.talero27@gmail.com>
2022-05-22 22:22:22 -0500

    Aprendimos a usar el async

---
+++ src/async/index.js
@@ -0,0 +1,31 @@
+const doSomeThingAsync = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+        ? setTimeout(() => resolve('Do Something Async'),3000)
+        : reject(new Error('Test Error'))
+    });
+}
+
+const doSomeThing= async () => {
+    const someThing = await doSomeThingAsync();
+    console.log(someThing);
+}
+
+console.log('Before');
+doSomeThing();
+console.log('After');
+
+
+const anotherFunction = async() => {
+    try {
+        const someThing = await doSomeThing();
+        console.log(someThing);
+    } catch(error) {
+
+    }
+}
+
+
+console.log('Before 1');
+anotherFunction();
+console.log('After 1');
